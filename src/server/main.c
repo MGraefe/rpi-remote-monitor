cc1c9454f4b9dc358dead740f6304dd72d54fbdf
Marius Gräfe <git@mgraefe.de>
2014-08-02 20:04:14 +0200

    reworked message buffer handling

--- src/server/main.c
+++ src/server/main.c
@@ -31,18 +31,37 @@ int g_listenSock = -1;
 enum servertype_e g_listenSockType;
 
 
-struct clientinfo_t
+struct clientinfo
 {
     int sock;
+    struct msgbuf sendbuf;
+    size_t sendbuf_pos;
 };
 
 size_t g_numClients = 0;
-struct clientinfo_t g_clientInfos[MAX_CLIENTS];
+struct clientinfo g_clientInfos[MAX_CLIENTS];
 
 struct measure_callback g_callbacks[32];
 struct measurement *g_measurements[32];
 size_t g_callbacks_num = 0;
 size_t g_measurements_num = 0;
+
+
+void clientinfo_init(struct clientinfo *info, int sock)
+{
+    info->sock = sock;
+    msgbuf_init(&info->sendbuf);
+    info->sendbuf_pos = 0;
+}
+
+
+void clientinfo_destruct(struct clientinfo *info)
+{
+    info->sock = -1;
+    msgbuf_dealloc(&info->sendbuf);
+    info->sendbuf_pos = 0;
+}
+
 
 void register_callbacks()
 {
@@ -205,11 +224,8 @@ void check_new_clients()
         fd_set fs_read;
         FD_ZERO(&fs_read);
         FD_SET(g_listenSock, &fs_read);
-        
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 0;
-        
+
+        struct timeval timeout = {0, 0};
         int fss = select(g_listenSock + 1, &fs_read, NULL, NULL, &timeout);
         if (fss <= 0)
             return;
@@ -236,7 +252,7 @@ void check_new_clients()
         
         print_connect_info(addr, addrsize);
         
-        g_clientInfos[g_numClients].sock = clsock;
+        clientinfo_init(&g_clientInfos[g_numClients], clsock);
         g_numClients++;
     }
 }
@@ -246,50 +262,81 @@ void disconnect_client(int index)
 {
     printf("A client disconnected\n");
     close(g_clientInfos[index].sock);
+    clientinfo_destruct(&g_clientInfos[index]);
     memmove(&g_clientInfos[index],
             &g_clientInfos[index + 1],
-            (g_numClients - index - 1) * sizeof(struct clientinfo_t));
+            (g_numClients - index - 1) * sizeof(struct clientinfo));
     g_numClients--;
 }
 
 
-int send_output(char *msg, size_t size)
+int _send(int sock, const char *data, size_t size)
 {
-    int fdmax = -1;
-    fd_set fs_write;
-    FD_ZERO(&fs_write);
-    for (size_t i = 0; i < g_numClients; i++)
+    return send(sock, data, size, MSG_NOSIGNAL);
+}
+
+
+//returns the number of bytes sent
+int send_to_client(struct clientinfo *client)
+{
+    if (client->sendbuf.size == 0)
+        return 0;
+    int sent = _send(client->sock,
+            client->sendbuf.data + client->sendbuf_pos,
+            client->sendbuf.size - client->sendbuf_pos);
+    if (sent <= 0)
+        return sent;
+    client->sendbuf_pos += sent;
+    if (client->sendbuf_pos == client->sendbuf.size) //all sent?
     {
-        int fd = g_clientInfos[i].sock;
-        FD_SET(fd, &fs_write);
-        fdmax = fd > fdmax ? fd : fdmax;
+        msgbuf_clear(&client->sendbuf);
+        client->sendbuf_pos = 0;
     }
-    
-    struct timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 100000;
-    
-    int numfds = select(fdmax + 1, NULL, &fs_write, NULL, &timeout);
-    if (numfds > 0)
-    {
-        //write to all clients
+    return sent;
+}
+
+
+int send_output()
+{
+    int repeat;
+    do {
+        int fdmax = -1;
+        fd_set fs_write;
+        FD_ZERO(&fs_write);
         for (size_t i = 0; i < g_numClients; i++)
         {
-            int sock = g_clientInfos[i].sock;
-            if (FD_ISSET(sock, &fs_write))
+            if (g_clientInfos[i].sendbuf.size > 0)
             {
-                //TODO: do some real stuff here
-                if (send(sock, msg, size, MSG_NOSIGNAL) <= 0)
-                    disconnect_client(i--);
+                int fd = g_clientInfos[i].sock;
+                FD_SET(fd, &fs_write);
+                fdmax = fd > fdmax ? fd : fdmax;
             }
         }
-    }
+
+        struct timeval timeout = {0, 0};
+        int numfds = select(fdmax + 1, NULL, &fs_write, NULL, &timeout);
+        if (numfds == 0)
+            break;
+    
+        repeat = 0;
+        for (size_t i = 0; i < g_numClients; i++)
+        {
+            if (FD_ISSET(g_clientInfos[i].sock, &fs_write))
+            {
+                int sent = send_to_client(&g_clientInfos[i]);
+                if (sent <= 0)
+                    disconnect_client(i--);
+                else if (g_clientInfos[i].sendbuf.size > 0)
+                    repeat = 1; //Still something left? repeat
+            }
+        }
+    } while (repeat);
     
     return 0;
 }
 
 
-void handle_clients()
+void main_loop()
 {
     struct msgbuf buf;
     msgbuf_init(&buf);
@@ -298,9 +345,20 @@ void handle_clients()
     {
         //always look if we have a new client
         check_new_clients();
+
+        //Clear our own messagebuffer and take the measurement
         msgbuf_clear(&buf);
         take_measurements(&buf);
-        send_output(buf.data, buf.size);
+
+        // Copy the measurement into the send-buffer of all clients IF their buffer is empty
+        // If their buffer is not empty they have some part of previous messages pending and
+        // simply are discarded from this update to prevent cluttering the server.
+        for (size_t i = 0; i < g_numClients; i++)
+            if (g_clientInfos[i].sendbuf.size == 0)
+                msgbuf_app(&g_clientInfos[i].sendbuf, buf.data, buf.size);
+
+        //Finally send the output to all clients and sleep
+        send_output();
         usleep(500000U);
     }
 }
@@ -311,7 +369,7 @@ int main()
     register_callbacks();
     init_measurements();
     start_server(29100, INET4, false);
-    handle_clients();
+    main_loop();
     
     return 0;
 }
